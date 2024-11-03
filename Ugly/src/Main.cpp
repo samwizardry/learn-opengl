dde5c07dd76606f0e63f7fba30771c9b22c3b93e
samwizardry <samwizardry@gmail.com>
2024-11-03 12:28:40 +0300

    Добавлен glad (opengl, core, 4.6)

--- Ugly/src/Main.cpp
+++ Ugly/src/Main.cpp
@@ -1,36 +1,42 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main()
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+    //GLFWwindow* window = glfwCreateWindow(640, 480, "Ugly", NULL, NULL);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    //if (!window)
+    //{
+    //    glfwTerminate();
+    //    return -1;
+    //}
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+    ///* Make the window's context current */
+    //glfwMakeContextCurrent(window);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+    ///* Loop until the user closes the window */
+    //while (!glfwWindowShouldClose(window))
+    //{
+    //    /* Render here */
+    //    glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+    //    /* Swap front and back buffers */
+    //    glfwSwapBuffers(window);
 
-    glfwTerminate();
+    //    /* Poll for and process events */
+    //    glfwPollEvents();
+    //}
+
+    //glfwTerminate();
 }
